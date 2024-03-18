f007c3d9040f02415d72e06a14d8835ea0e62b03
ashleypeters182 <ashleypeters182@gmail.com>
2024-03-18 11:32:04 -0400

    started pseudocode

---
+++ chassis_movement/chassis.py
@@ -0,0 +1,34 @@
+# some description of what the code does
+
+
+# package imports
+import serial
+from timeit import default_timer
+
+# code here
+
+
+# initialization
+    # send signal to chassis motors
+    # if signal is returned - break loop
+    # if not, loop x amount of times before sending an error message
+
+# calibration
+
+
+# defining emergency stopping timer
+
+
+# main loop here
+start = default_timer()
+    # data collection
+        # move chassis forward slowly
+
+        duration = default_timer() - start
+
+        # if duration >= x, break loop
+        # if duration is < x, continue
+
+    # system reset
+        # reverse all motors to move the system back to starting position
+        # this will be done faster than the speed at which the system scans
