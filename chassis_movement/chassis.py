76a3dac23f12ff0db5a9adcef57e1af8150df8ae
Kayla-Ng <161973697+Kayla-Ng@users.noreply.github.com>
2024-04-11 12:39:11 -0400

    Update chassis.py

--- chassis_movement/chassis.py
+++ chassis_movement/chassis.py
@@ -7,7 +7,7 @@ M3 = motor controller 3 --> left back wheels
 M4 = motor controller 4 --> right back wheels
 """
 
-# package imports
+# Package imports
 import Adafruit_PCA9685
 import time
     # import serial
@@ -26,79 +26,45 @@ pwm = Adafruit_PCA9685.PCA9685(busnum=1)
 pwm.set_pwm_freq(60)
 
 #Intializing Motor Parameters
-min_signal = 1050  # Minimum pulse length out of 4096
-max_signal = 2050  # Maximum pulse length out of 4096
 MC1 = 0 
 MC2 = 1
 MC3 = 2
 MC4 = 3
 
-# Convert microseconds to PWM values
-min_pwm = microseconds_to_pwm(min_signal, 60)
-max_pwm = microseconds_to_pwm(max_signal, 60)
-print(min_pwm)
-print(max_pwm)
+# Define the signal values for 100%, 0%, and -100% speed respectively
+max_signal = 2050
+stop_signal = 1550
+min_signal = 1050
 
-"""
-Old code, left in case we need...
-GPIO.setmode(GPIO.BOARD)
-MC1 = 32
-MC2 = 33
-GPIO.setup(MC1, GPIO.OUT)
-GPIO.setup(MC2, GPIO.OUT)
-Freq = 50
-Duty_cycle = 50
-MC1_pwm = GPIO.PWM(MC1, Freq)
-MC2_pwm = GPIO.PWM(MC2, Freq)
-""" 
+# Mapping Percent and Signal Values
+def percent_to_pwm(percent):
+    if percent > 100: 
+        percent = 100
+    elif percent < -100:
+        percent = -100
+
+    # Calculate the PWM signal value based on the provided percent speed
+    signal_value = stop_signal + (percent / 100) * (max_signal - stop_signal)
+
+    # Convert microseconds to PWM values
+    pwm_value = microseconds_to_pwm(signal_value, 60)
+
+    return int(pwm_value)
 
 # Motor Move Function
-def motormove(controller, 0, speed)
-pwm.set_pwm(controller, 0, speed)
+def motor_move(controller, 0, speed):
+    pwm.set_pwm(controller, 0, speed)
 
-""" 
-Need to think about: 
-    * how to define speed
-""" 
-
-# Chassis Forward Function
-def chassisforward(speed)
-motormove(MC1, 0, speed)
-motormove(MC2, 0, speed)
-motormove(MC3, 0, speed)
-motormove(MC4, 0, speed)
-
-# Chassis Backward Function 
-def chassisbackward(speed)
-motormove(MC1, 0, speed)
-motormove(MC2, 0, speed)
-motormove(MC3, 0, speed)
-motormove(MC4, 0, speed)
-
-# Chassis Stop Function
-"""
-Look into how to stop all controllers at once
-def motorstop(controller, 0);
-    neutral_signal = 1550  # Neutral position for many servos and ESCs
-    neutral_pwm = microseconds_to_pwm(neutral_signal, 60)
-    print("Setting motor to neutral")
-    pwm.set_pwm(controller, 0, neutral_pwm)
-    time.sleep(2)
-"""
-
-
-# initialization
-    # send signal to chassis motors
-    # if signal is returned - break loop
-    # if not, loop x amount of times before sending an error message
-
-# calibration
-
-
-# defining emergency stopping timer
-
+# Move Chassis Function
+def chassis_move(speed):    #speed percent value
+    percent_to_pwm(speed)
+    motormove(MC1, 0, pwm_value)
+    motormove(MC2, 0, pwm_value)
+    motormove(MC3, 0, pwm_value)
+    motormove(MC4, 0, pwm_value)
 
 # main loop here
+
 start = default_timer()
     # data collection
         # move chassis forward slowly
