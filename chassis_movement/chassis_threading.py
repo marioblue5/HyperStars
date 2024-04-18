e142bb90bd09d75dfb592c74850c82e362f90ec1
Mario Luis Molina <148909335+marioblue5@users.noreply.github.com>
2024-04-18 16:01:38 -0400

    Update chassis_threading.py

--- chassis_movement/chassis_threading.py
+++ chassis_movement/chassis_threading.py
@@ -238,13 +238,13 @@ pwm.set_pwm_freq(60)
 if __name__ == '__main__':
     try:
         steps = steps_per_revolution * 3  # Change "1" to adjust the number of revolutions
-        thread1 = threading.Thread(target=chassis_forward_backward,args=(10,10))
+        thread1 = threading.Thread(target=chassis_forward_backward,args=(8,25))
         thread2 = threading.Thread(target=move_stepmotor,args=(True,steps))
         thread1.start()
         thread2.start()
         
         thread1.join()
-        chassis_forward_backward(10,-10)
+        chassis_forward_backward(10,-25)
         thread2.join()
         time.sleep(2)
         move_stepmotor(False, steps)  # Move backward
