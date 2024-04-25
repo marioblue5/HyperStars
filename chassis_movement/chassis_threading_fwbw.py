aa44828aeb522e20c2305683c20c834dcd6ad8ae
Mario Luis Molina <148909335+marioblue5@users.noreply.github.com>
2024-04-25 19:33:40 -0400

    Update chassis_threading_fwbw.py

--- chassis_movement/chassis_threading_fwbw.py
+++ chassis_movement/chassis_threading_fwbw.py
@@ -35,8 +35,6 @@ def ramping(percent_speed):
     # Create an increasing ramp from 0 to percent_speed
     increasing_ramp = np.linspace(0, percent_speed, num=intervals, endpoint=True)
 
-    #Remove the initial value becaue it's redundant
-    increasing_ramp = increasing_ramp[1:]
 
     # Create a decreasing ramp from percent_speed to 0, use less intervals since it'll be easier to slow down
     decreasing_ramp = np.linspace(percent_speed, 0, num=intervals-2, endpoint=True)
