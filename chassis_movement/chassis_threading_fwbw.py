d98da865bcbec8035976975fc0936b84a5354393
Mario Luis Molina <148909335+marioblue5@users.noreply.github.com>
2024-04-25 19:53:38 -0400

    Update chassis_threading_fwbw.py

--- chassis_movement/chassis_threading_fwbw.py
+++ chassis_movement/chassis_threading_fwbw.py
@@ -322,10 +322,10 @@ pwm.set_pwm_freq(60)
 if __name__ == '__main__':
     try:
         steps = steps_per_revolution * 3  # Change "1" to adjust the number of revolutions
-        thread1 = threading.Thread(target=chassis_forward_backward,args=(15,15))
-        # thread2 = threading.Thread(target=start_capture)
+        thread1 = threading.Thread(target=chassis_forward_backward,args=(8,15))
+        thread2 = threading.Thread(target=start_capture)
         thread1.start()
-        # thread2.start()
+        thread2.start()
         
         thread1.join()
         # thread2.join()
